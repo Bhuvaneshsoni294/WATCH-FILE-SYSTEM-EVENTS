4f1e2f25b0c0475b277be444b21ac10daf667cea
Bhuvaneshsoni294 <89522795+Bhuvaneshsoni294@users.noreply.github.com>
2023-02-25 19:10:04 +0530

    Update file_system_events_tracker.py

--- file_system_events_tracker.py
+++ file_system_events_tracker.py
@@ -13,18 +13,16 @@ from_dir = "C:/Users/hp/Downloads"
 
 # Event Hanlder Class
 class FileEventHandler(FileSystemEventHandler):
-    class FileEventHandler(FileSystemEventHandler):
-#1_on_created
-      def on_created (self, event): 
+    def on_created (self, event): 
         print(f"Hey, {event.src_path} has been created!")
 #2_on_deleted
-      def on_deleted (self,event):
-         print (f"Oops! Someone deleted {event.src_path}!")
+    def on_deleted (self,event):
+        print (f"Oops! Someone deleted {event.src_path}!")
  #3_on_modified     
-      def on_modified (self,event):
-         print (f"Hey there!, {event.src_path} has been modified")
+    def on_modified (self,event):
+        print (f"Hey there!, {event.src_path} has been modified")
   #4_on_moved     
-      def on_moved (self,event):
+    def on_moved (self,event):
         print(f"Someone moved {event.src_path} to {event.dest_path}")
 
 # Initialize Event Handler Class
@@ -49,13 +47,3 @@ try:
 except KeyboardInterrupt:
      print("stopped!") 
      observer.stop()
-
-while True:
-    time.sleep(2)
-    print("running...")
-
-
-
-
-
-
